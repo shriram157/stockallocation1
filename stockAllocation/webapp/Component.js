bb0594b65e660f75aa9edf383ab04149a2ad5db1
Anthony Wat <anthony_wat@toyota.ca>
2019-05-24 09:45:12 -0400

    Added session timeout dialog

--- stockAllocation/webapp/Component.js
+++ stockAllocation/webapp/Component.js
@@ -1,10 +1,12 @@
 sap.ui.define([
+	"sap/m/Dialog",
+	"sap/m/Text",
 	"sap/ui/core/UIComponent",
 	"sap/ui/Device",
 	"suggestOrder/model/models",
 	"./model/errorHandling",
-		"sap/ui/model/odata/v2/ODataModel"
-], function (UIComponent, Device, models, errorHandling, ODataModel) {
+	"sap/ui/model/odata/v2/ODataModel"
+], function (Dialog, Text, UIComponent, Device, models, errorHandling, ODataModel) {
 	"use strict";
 
 	var navigationWithContext = {
@@ -25,18 +27,43 @@ sap.ui.define([
 		init: function () {
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
+
+			// Get resource bundle
+			var bundle = this.getModel('i18n').getResourceBundle();
+
+			// Attach XHR event handler to detect 401 error responses for handling as timeout
+			var sessionExpDialog = new Dialog({
+				title: bundle.getText('SESSION_EXP_TITLE'),
+				type: 'Message',
+				state: 'Warning',
+				content: new Text({
+					text: bundle.getText('SESSION_EXP_TEXT')
+				})
+			});
+			var origOpen = XMLHttpRequest.prototype.open;
+			XMLHttpRequest.prototype.open = function () {
+				this.addEventListener('load', function (event) {
+					// TODO Compare host name in URLs to ensure only app resources are checked
+					if (event.target.status === 401) {
+						if (!sessionExpDialog.isOpen()) {
+							sessionExpDialog.open();
+						}
+					}
+				});
+				origOpen.apply(this, arguments);
+			};
+
 			// set the FLP model
 			this.setModel(models.createFLPModel(), "FLP");
-//
 
-            var sLocation = window.location.host;
+			var sLocation = window.location.host;
 			var sLocation_conf = sLocation.search("webide");
 
 			var mConfig = this.getMetadata().getManifestEntry("/sap.app/dataSources/ZCDS_SUGGEST_ORD_CDS");
-				if (sLocation_conf == 0) {
-				mConfig.uri = "/Suggest_Order" + mConfig.uri;  //
-			} else  {
-					mConfig.uri = mConfig.uri;
+			if (sLocation_conf == 0) {
+				mConfig.uri = "/Suggest_Order" + mConfig.uri;
+			} else {
+				mConfig.uri = mConfig.uri;
 			}
 			var oDataModel = new ODataModel(mConfig.uri, {
 				useBatch: false,
@@ -48,14 +75,14 @@ sap.ui.define([
 				}
 			});
 			this.setModel(oDataModel, "ZCDS_SUGGEST_ORD_CDS");
-			
-// the Summation Data model. 
+
+			// the Summation Data model. 
 
 			var mConfig = this.getMetadata().getManifestEntry("/sap.app/dataSources/ZCDS_SUGGEST_ORD_SUM_CDS");
-				if (sLocation_conf == 0) {
-				mConfig.uri = "/Suggest_Order" + mConfig.uri;  //ecpSales_node_secured
-			} else  {
-					mConfig.uri = mConfig.uri;
+			if (sLocation_conf == 0) {
+				mConfig.uri = "/Suggest_Order" + mConfig.uri; //ecpSales_node_secured
+			} else {
+				mConfig.uri = mConfig.uri;
 			}
 			var oDataModel = new ODataModel(mConfig.uri, {
 				useBatch: false,
@@ -67,14 +94,14 @@ sap.ui.define([
 				}
 			});
 			this.setModel(oDataModel, "ZCDS_SUGGEST_ORD_SUM_CDS");
-			
-//the count data model. ZCDS_SUGGEST_ORD_COUNT_CDS
+
+			//the count data model. ZCDS_SUGGEST_ORD_COUNT_CDS
 
 			var mConfig = this.getMetadata().getManifestEntry("/sap.app/dataSources/ZCDS_SUGGEST_ORD_COUNT_CDS");
-				if (sLocation_conf == 0) {
-				mConfig.uri = "/Suggest_Order" + mConfig.uri;  
-			} else  {
-					mConfig.uri = mConfig.uri;
+			if (sLocation_conf == 0) {
+				mConfig.uri = "/Suggest_Order" + mConfig.uri;
+			} else {
+				mConfig.uri = mConfig.uri;
 			}
 			var oDataModel = new ODataModel(mConfig.uri, {
 				useBatch: false,
@@ -87,12 +114,12 @@ sap.ui.define([
 			});
 			this.setModel(oDataModel, "ZCDS_SUGGEST_ORD_COUNT_CDS");
 
- //ZSD_SUGGEST_ORDER_UPDATE_SRV
- 			var mConfig = this.getMetadata().getManifestEntry("/sap.app/dataSources/ZSD_SUGGEST_ORDER_UPDATE_SRV");
-				if (sLocation_conf == 0) {
-				mConfig.uri = "/Suggest_Order" + mConfig.uri;  
-			} else  {
-					mConfig.uri = mConfig.uri;
+			//ZSD_SUGGEST_ORDER_UPDATE_SRV
+			var mConfig = this.getMetadata().getManifestEntry("/sap.app/dataSources/ZSD_SUGGEST_ORDER_UPDATE_SRV");
+			if (sLocation_conf == 0) {
+				mConfig.uri = "/Suggest_Order" + mConfig.uri;
+			} else {
+				mConfig.uri = mConfig.uri;
 			}
 			var oDataModel = new ODataModel(mConfig.uri, {
 				useBatch: false,
@@ -104,10 +131,6 @@ sap.ui.define([
 				}
 			});
 			this.setModel(oDataModel, "ZSD_SUGGEST_ORDER_UPDATE_SRV");
- 
- 
- 
- 
 
 			// set the dataSource model
 			this.setModel(new sap.ui.model.json.JSONModel({}), "dataSource");
