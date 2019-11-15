c27b918de12f840c90b8c2bca8b6a42695fb7538
Aarti Dhamat <aarti_dhamat@toyota.ca>
2019-11-15 16:08:00 +0000

    toggle "0" requested/suggested

--- stockAllocation/webapp/controller/StockAllocation.controller.js
+++ stockAllocation/webapp/controller/StockAllocation.controller.js
@@ -148,14 +148,14 @@
 				if (evt.getSource().getPressed() == false) {
 					console.log("pressed", evt);
 					for (var i = 0; i < oModelData2.length; i++) {
-						if ((oModelData2[i].suggested <= 0) && (oModelData2[i].suggested <= "0%")) {
+						if ((oModelData2[i].suggested <= 0) && (oModelData2[i].suggested <= "0")) {
 							oModelData2[i].visibleProperty = false;
 						}
 					}
 				} else if (evt.getSource().getPressed() == true) {
 					console.log("pressed2", evt);
 					for (var i = 0; i < oModelData2.length; i++) {
-						if ((oModelData2[i].suggested <= 0) && (oModelData2[i].suggested <= "0%")) {
+						if ((oModelData2[i].suggested <= 0) && (oModelData2[i].suggested <= "0")) {
 							oModelData2[i].visibleProperty = true;
 						}
 					}
