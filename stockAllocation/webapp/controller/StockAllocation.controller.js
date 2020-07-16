ae88b142436531e468fee13b85bea85a5ffa4980
Aarti Dhamat <aarti_dhamat@toyota.ca>
2020-07-16 18:37:37 +0000

    dealer flag fix-hypercare

--- stockAllocation/webapp/controller/StockAllocation.controller.js
+++ stockAllocation/webapp/controller/StockAllocation.controller.js
@@ -260,8 +260,10 @@
 				//objNew.ZzuiFlag 
 				if (oCheck.getParameter("selected") == true) {
 					oCheck.getSource().getBindingContext("stockDataModel").getObject().ZzuiFlag = "Y";
+					oCheck.getSource().getBindingContext("stockDataModel").getObject().zzuiFlag = "Y";
 				} else {
 					oCheck.getSource().getBindingContext("stockDataModel").getObject().ZzuiFlag = "N";
+					oCheck.getSource().getBindingContext("stockDataModel").getObject().zzuiFlag = "N";
 				}
 				this.getView().getModel("stockDataModel").updateBindings(true);
 			},
