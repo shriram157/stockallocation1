6bb52266b87d1cd76b9010fbd5eaf35f302c2828
Minakshi Singh <minakshi_singh@toyota.ca>
2020-09-20 11:21:48 +0000

    changes done for showing suggested tab

--- stockAllocation/webapp/controller/ProductionRequestSummary.controller.js
+++ stockAllocation/webapp/controller/ProductionRequestSummary.controller.js
@@ -1396,53 +1396,53 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
             var suggestedModelLength = oSuggestedModel.getData().length;
             
             
-			// if (oSuggestedModel) {
+			if (oSuggestedModel) {
 				
-			// 	// loop and count the suggested model where qty is greater than 0. 					
+				// loop and count the suggested model where qty is greater than 0. 					
 				
 
-			// 	// 					for (var i = elements.length - 1; i >= 0; i--) {
-			// 	//   if (elements[i] == remove) {
-			// 	//     elements.splice(i, 1);
-			// 	//   }
-			// 	// }
+				// 					for (var i = elements.length - 1; i >= 0; i--) {
+				//   if (elements[i] == remove) {
+				//     elements.splice(i, 1);
+				//   }
+				// }
 
-			// 	for (var i = suggestedModelData.length - 1; i >= 0; i--) {
-			// 		if (suggestedModelData[i].zzdel_review == "Y" && validWindowSuggested == true) {
+				for (var i = suggestedModelData.length - 1; i >= 0; i--) {
+					if (suggestedModelData[i].zzdel_review == "Y" && validWindowSuggested == true) {
 
-			// 			if (suggestedModelData[i].suggestedVolume <= 0) {
-			// 				suggestedModelLength = suggestedModelLength - 1;
-			// 				var volumeZeroRecord = true;
-			// 			} else {
-			// 				var volumeZeroRecord = false;
-			// 			}
+						if (suggestedModelData[i].suggestedVolume <= 0) {
+							suggestedModelLength = suggestedModelLength - 1;
+							var volumeZeroRecord = true;
+						} else {
+							var volumeZeroRecord = false;
+						}
 
-			// 			suggestedModelData.splice(i, 1);
-			// 			if (volumeZeroRecord == false) {
-			// 				suggestedModelLength = suggestedModelLength - 1;
-			// 			}
+						suggestedModelData.splice(i, 1);
+						if (volumeZeroRecord == false) {
+							suggestedModelLength = suggestedModelLength - 1;
+						}
 
-			// 		}
-			// 	}
-			// 	for (var i = 0; i < suggestedModelData.length; i++) {
+					}
+				}
+				for (var i = 0; i < suggestedModelData.length; i++) {
 
-			// 		if (suggestedModelData[i].suggestedVolume <= 0) {
-			// 			suggestedModelLength = suggestedModelLength - 1;
-			// 		}
-			// 	}
+					if (suggestedModelData[i].suggestedVolume <= 0) {
+						suggestedModelLength = suggestedModelLength - 1;
+					}
+				}
 
-			// 	// }
-			// 	oSuggestedModel.updateBindings(true);
+				// }
+				oSuggestedModel.updateBindings(true);
 
-			// 	// check if suggested model has any anydata then only publish the tab suggestedTAb otherwise turn it off. 
-			// 	if (validWindowSuggested == true) {
-			// 		var suggestedModelLengthTemp = oSuggestedModel.getData().length;
-			// 		if (suggestedModelLengthTemp <= 0) {
-			// 			oModel.setProperty("/showSuggestionTab", false);
-			// 		}
-			// 	}
+				// check if suggested model has any anydata then only publish the tab suggestedTAb otherwise turn it off. 
+				// if (validWindowSuggested == true) {
+				// 	var suggestedModelLengthTemp = oSuggestedModel.getData().length;
+				// 	if (suggestedModelLengthTemp <= 0) {
+				// 		oModel.setProperty("/showSuggestionTab", false);
+				// 	}
+				// }
 
-			// }
+			}
 			
 			
 			var oRequestedModel = this.getView().getModel("requestedDataModel");
