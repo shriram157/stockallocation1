1cefe669e6ac5369e828be44ab04cff686ddc66c
Minakshi Singh <minakshi_singh@toyota.ca>
2022-05-30 12:55:16 +0000

    changes done for adding custom control

---
+++ stockAllocation/webapp/controls/CustomStepInput.js
@@ -0,0 +1,21 @@
+sap.ui.define(['sap/m/StepInput'],
+function(StepInput){
+	'use strict';
+	var CustomStepInput = StepInput.extend("suggestOrder.controls.CustomStepInput", {
+		metadata : {
+			events : {
+				"change" : {
+				
+				}
+			}
+		},
+		onmousedown: function(evt){
+			console.log(evt);
+		},
+	
+		renderer :"sap.m.InputRenderer"	
+		
+	});
+	
+	return CustomStepInput;
+});
