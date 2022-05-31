40c4b02b3e6d1eaa693ae488772ad6e5610c0762
Minakshi Singh <minakshi_singh@toyota.ca>
2022-05-31 09:42:18 +0000

    changes done for step counts

--- stockAllocation/webapp/controls/CustomStepInput.js
+++ stockAllocation/webapp/controls/CustomStepInput.js
@@ -10,9 +10,16 @@ function(StepInput){
 			}
 		},
 		onmousedown: function(evt){
-			 console.log(this.mEventRegistry.change[0].oListener.obj.reqThreshold);
-			 this.setMax(this.mEventRegistry.change[0].oListener.obj.reqThreshold);
+			 //console.log(this.mEventRegistry.change[0].oListener.obj.reqThreshold);
+			 //this.setMax(this.mEventRegistry.change[0].oListener.obj.reqThreshold);
 			 //StockAllocation.whenUserChangesRequestedData();
+			 
+			 var oArr = this.getParent().getParent().getModel("stockDataModel").getData();
+			 var nCount = oArr.filter(elm => elm.modelCodeDescription === this.getParent().getCells()[0].getText()).length 
+			 + Number(this.mEventRegistry.change[0].oListener.allowedtolerance);
+			 
+			 this.setMax(nCount);
+			 
 		},
 	
 		renderer :"sap.m.StepInputRenderer"	
