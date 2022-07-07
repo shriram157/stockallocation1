5a02497b2af8152ba45545658e1e67707da2c0c9
Swetha Vatsavai <swetha_vatsavai@toyota.ca>
2022-07-07 16:15:49 +0000

    check box

--- stockAllocation/webapp/controller/StockAllocation.controller.js
+++ stockAllocation/webapp/controller/StockAllocation.controller.js
@@ -467,7 +467,7 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 				}
 
 				//HyperCare 3.0
-				if (Zzui_Flag = "Y") {
+				if (Zzui_Flag == "Y") {
 					if (Number(currentData.requested_Volume) < Number(currentData.suggested) {
 						currentData.checkBoxEnabled = true;
 						currentData.checkBoxFlag = true;
